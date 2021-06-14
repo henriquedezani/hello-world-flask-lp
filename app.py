a938d1e6ba24f7d7016b6feb62a42e7ec3cb2919
Henrique Dezani <dezani@gmail.com>
2021-06-14 19:32:26 +0000

    Implementado consulta e inserção usando HTML

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
 app = Flask(__name__)
+
+contatos = [
+    {'nome': 'Fulano', 'telefone': '123123123'},
+    {'nome': 'Ciclano', 'telefone': '423234234'},
+    {'nome': 'Beltrano', 'telefone': '6787687686'}
+]
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    # contatos = SELECT ...
+    # abre conexão, executa SQL, fecha conexão
+    return render_template("index.html", contatos=contatos, quantidade=len(contatos))
+
+@app.route('/create')
+def create():
+    return render_template("create.html")
+
+@app.route('/save', methods=['POST'])
+def save():
+    nome = request.form['nome']
+    telefone = request.form['telefone']
+
+    novo_contato = {'nome': nome, 'telefone': telefone}
+    # abre conexão, executa SQL insert, fecha conexão
+    contatos.append(novo_contato) # INSERT INTO contatos VALUES (...)
+
+    return redirect("https://5000-turquoise-pinniped-63qr3oo1.ws-us09.gitpod.io")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
