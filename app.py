b819b0321997c5218700c465abdb6f63c9caf1d7
Henrique Dezani <dezani@gmail.com>
2021-06-18 17:02:24 +0000

    Adicionado SQLite

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
-contatos = [
-    {'nome': 'Fulano', 'telefone': '123123123'},
-    {'nome': 'Ciclano', 'telefone': '423234234'},
-    {'nome': 'Beltrano', 'telefone': '6787687686'}
-]
+def create_database():
+    conn = sqlite3.connect('agenda.db')
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS Contatos(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome VARCHAR NOT NULL, telefone VARCHAR NOT NULL)""")
+    conn.close()
 
 @app.route('/')
 def hello():
     # contatos = SELECT ...
     # abre conexão, executa SQL, fecha conexão
+    conn = sqlite3.connect('agenda.db')
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * FROM Contatos """)
+    contatos = cursor.fetchall()        
     return render_template("index.html", contatos=contatos, quantidade=len(contatos))
+    conn.close()
 
 @app.route('/create')
 def create():
@@ -23,11 +29,16 @@ def save():
     nome = request.form['nome']
     telefone = request.form['telefone']
 
-    novo_contato = {'nome': nome, 'telefone': telefone}
+    # novo_contato = {'nome': nome, 'telefone': telefone}
+    # contatos.append(novo_contato) # INSERT INTO contatos VALUES (...)
     # abre conexão, executa SQL insert, fecha conexão
-    contatos.append(novo_contato) # INSERT INTO contatos VALUES (...)
+    conn = sqlite3.connect('agenda.db')
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO Contatos(nome, telefone) VALUES(?, ?) """, (nome, telefone))
+    conn.commit()
 
     return redirect("/")
 
 if __name__ == '__main__':
+    create_database()
     app.run(debug=True)
